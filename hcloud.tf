d204b063052c43ef4e08dba22d793bc1ea28e218
K8H <k8hofmann@gmail.com>
2024-12-14 12:10:35 +0100

    add generic-ssh-key

--- hcloud.tf
+++ hcloud.tf
@@ -11,8 +11,16 @@ terraform {
 variable "hcloud_token" {
   sensitive = true
 }
-
-# Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = var.hcloud_token
+}
+
+resource "tls_private_key" "generic-ssh-key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "hcloud_ssh_key" "primary-ssh-key" {
+  name = "primary-ssh-key"
+  public_key = tls_private_key.generic-ssh-key.public_key_openssh
 }
